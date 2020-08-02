1d8743fc77b6941105b3e5362f99d51c874b082b
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-08-02 14:24:32 +0800

    [web] add enternal link component

---
+++ packages/web/components/external-link.vue
@@ -0,0 +1,24 @@
+<template>
+  <a
+    class="font-weight-medium text-decoration-none"
+    :href="href"
+    referrerpolicy="no-referrer"
+    rel="noopener noreferrer"
+    target="_blank"
+  >
+    <slot />
+  </a>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    href: {
+      required: true,
+      type: String,
+    },
+  },
+})
+</script>
