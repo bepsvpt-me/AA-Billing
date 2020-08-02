b84910254bcc028f55685b3c37399808f66d4919
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-08-02 11:23:38 +0800

    [web] init dash, found and join pages

---
+++ packages/web/pages/dash/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>dash</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data: () => ({}),
+})
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
---
+++ packages/web/pages/join/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>join</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data: () => ({}),
+})
+</script>
+
+<style lang="scss" scoped>
+//
+</style>
