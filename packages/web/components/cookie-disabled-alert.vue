ce833e1067ce76d7bd47e2c49395a454e6a23e3b
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-08-02 14:25:17 +0800

    [web] add cookie is disabled alert

---
+++ packages/web/components/cookie-disabled-alert.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-snackbar v-if="open" bottom color="error" timeout="-1" :value="true">
+    <span>您的瀏覽器已停用 Cookie，網站部分功能可能無法正常運作。</span>
+
+    <external-link v-if="link" class="link" :href="link">
+      了解詳情
+    </external-link>
+
+    <template #action="{ attrs }">
+      <v-btn text v-bind="attrs" @click="open = false">
+        關閉
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import ExternalLink from '~/components/external-link.vue'
+
+export default Vue.extend({
+  components: {
+    ExternalLink,
+  },
+
+  data() {
+    return {
+      open: !window.navigator.cookieEnabled,
+    }
+  },
+
+  computed: {
+    link(): string | undefined {
+      switch (this.$ua.browser().toLowerCase()) {
+        case 'chrome':
+          return 'https://support.google.com/chrome/answer/95647?hl=zh-Hant'
+
+        case 'safari':
+          return 'https://support.apple.com/zh-tw/guide/safari/sfri11471/mac'
+
+        case 'firefox':
+          return 'https://support.mozilla.org/zh-TW/kb/websites-say-cookies-are-blocked-unblock-them'
+
+        default:
+          return undefined
+      }
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.link {
+  color: #1867c0;
+}
+</style>
--- packages/web/layouts/default.vue
+++ packages/web/layouts/default.vue
@@ -5,16 +5,22 @@
     </v-main>
 
     <app-footer />
+
+    <client-only>
+      <cookie-disabled-alert />
+    </client-only>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import AppFooter from '~/components/footer/index.vue'
+import CookieDisabledAlert from '~/components/cookie-disabled-alert.vue'
 
 export default Vue.extend({
   components: {
     AppFooter,
+    CookieDisabledAlert,
   },
 })
 </script>
