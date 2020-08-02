648ba7aae375bec7e4907e0f9492a6013ee34e39
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-08-02 12:09:19 +0800

    [web] fallback to memory storage when browser storage is not available

--- packages/web/plugins/vuex-persist.client.ts
+++ packages/web/plugins/vuex-persist.client.ts
@@ -1,8 +1,23 @@
 import { Plugin } from '@nuxt/types'
-import VuexPersistence from 'vuex-persist'
+import VuexPersistence, { PersistOptions } from 'vuex-persist'
+// @ts-ignore
+import MemoryStorage from 'memorystorage'
+
+const storage = () => {
+  try {
+    return window.localStorage
+  } catch (_) {
+    return new MemoryStorage()
+  }
+}
 
 const persist: Plugin = ({ store }) => {
-  new VuexPersistence({ key: 'aa-billing' }).plugin(store)
+  const options: PersistOptions<Record<string, any>> = {
+    key: 'aa-billing',
+    storage: storage(),
+  }
+
+  new VuexPersistence(options).plugin(store)
 }
 
 export default persist
