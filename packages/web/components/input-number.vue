5fa2e9a802122681102e75f350ac6efaa70c0655
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-08-02 22:00:09 +0800

    [web] add input-number component

---
+++ packages/web/components/input-number.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-text-field
+    v-bind="$attrs"
+    class="number-input"
+    type="number"
+    :value="value"
+    @input="$emit('input', $event)"
+    @keydown.down.prevent=""
+    @keydown.up.prevent=""
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    value: {
+      required: true,
+      type: String,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.number-input::v-deep input[type='number'] {
+  -moz-appearance: textfield;
+
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+}
+</style>
