1674533739dd76e84b99dfc146355a09c4ff028e
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-08-05 20:31:13 +0800

    [web] add maximum members to plan and add types

--- packages/web/components/found/form/services.ts
+++ packages/web/components/found/form/services.ts
@@ -1,34 +1,49 @@
+export interface Plan {
+  text: string
+  value: string
+  currency: 'TWD' | 'USD'
+  maximum: string
+}
+
+export interface Service {
+  text: string
+  value: string
+  plans: Plan[]
+}
+
 export default [
   {
     text: '1Password Families',
     value: '1password',
-    plans: [{ text: '家庭方案（年）', value: '59.88', currency: 'USD' }],
+    plans: [
+      { text: '家庭方案（年）', value: '59.88', currency: 'USD', maximum: '5' },
+    ],
   },
   {
     text: 'Microsoft 365 Family',
     value: 'microsoft-365',
     plans: [
-      { text: '家庭方案（年）', value: '3190', currency: 'TWD' },
-      { text: '家庭方案（月）', value: '320', currency: 'TWD' },
+      { text: '家庭方案（年）', value: '3190', currency: 'TWD', maximum: '6' },
+      { text: '家庭方案（月）', value: '320', currency: 'TWD', maximum: '6' },
     ],
   },
   {
     text: 'Netflix',
     value: 'netflix',
     plans: [
-      { text: '高級', value: '390', currency: 'TWD' },
-      { text: '標準', value: '330', currency: 'TWD' },
-      { text: '基本', value: '270', currency: 'TWD' },
+      { text: '高級', value: '390', currency: 'TWD', maximum: '4' },
+      { text: '標準', value: '330', currency: 'TWD', maximum: '2' },
+      { text: '基本', value: '270', currency: 'TWD', maximum: '1' },
     ],
   },
   {
     text: 'Spotify Premium Family',
     value: 'spotify',
-    plans: [{ text: '家庭方案', value: '240', currency: 'TWD' }],
+    plans: [{ text: '家庭方案', value: '240', currency: 'TWD', maximum: '6' }],
   },
   {
     text: 'YouTube Premium',
     value: 'youtube',
-    plans: [{ text: '家庭方案', value: '270', currency: 'TWD' }],
+    plans: [{ text: '家庭方案', value: '270', currency: 'TWD', maximum: '6' }],
   },
-]
+] as Service[]
