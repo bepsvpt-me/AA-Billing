1a96abac9a0f2757828342632c9aade9f121ad2c
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-08-05 19:25:25 +0800

    [web] add service currency

--- packages/web/components/found/form/services.ts
+++ packages/web/components/found/form/services.ts
@@ -2,30 +2,33 @@ export default [
   {
     text: '1Password Families',
     value: '1password',
-    plans: [{ text: '家庭方案', value: '4.99' }],
+    plans: [{ text: '家庭方案（年）', value: '59.88', currency: 'USD' }],
   },
   {
     text: 'Microsoft 365 Family',
     value: 'microsoft-365',
-    plans: [{ text: '家庭方案', value: '3190' }],
+    plans: [
+      { text: '家庭方案（年）', value: '3190', currency: 'TWD' },
+      { text: '家庭方案（月）', value: '320', currency: 'TWD' },
+    ],
   },
   {
     text: 'Netflix',
     value: 'netflix',
     plans: [
-      { text: '高級', value: '390' },
-      { text: '標準', value: '330' },
-      { text: '基本', value: '270' },
+      { text: '高級', value: '390', currency: 'TWD' },
+      { text: '標準', value: '330', currency: 'TWD' },
+      { text: '基本', value: '270', currency: 'TWD' },
     ],
   },
   {
     text: 'Spotify Premium Family',
     value: 'spotify',
-    plans: [{ text: '家庭方案', value: '240' }],
+    plans: [{ text: '家庭方案', value: '240', currency: 'TWD' }],
   },
   {
     text: 'YouTube Premium',
     value: 'youtube',
-    plans: [{ text: '家庭方案', value: '270' }],
+    plans: [{ text: '家庭方案', value: '270', currency: 'TWD' }],
   },
 ]
